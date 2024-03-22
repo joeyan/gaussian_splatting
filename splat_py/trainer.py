17d661fcd6fac5e52d33231611f1d9ed74401cd3
Joe Yan <yanzhizhong@gmail.com>
2024-03-22 18:36:51 -0400

    move training loop into trainer

--- splat_py/trainer.py
+++ splat_py/trainer.py
@@ -1,14 +1,49 @@
+import numpy as np
+import cv2
+
 import torch
+from torchmetrics.image import StructuralSimilarityIndexMeasure
 
 from splat_py.constants import *
-from splat_py.utils import inverse_sigmoid, quaternion_to_rotation_torch
+from splat_py.cuda_autograd_functions import (
+    CameraPointProjection,
+    ComputeSigmaWorld,
+    ComputeProjectionJacobian,
+    ComputeSigmaImage,
+    RenderImage,
+)
+from splat_py.structs import Gaussians, Tiles, SimpleTimer
+from splat_py.tile_culling import (
+    match_gaussians_to_tiles_gpu,
+    sort_gaussians,
+)
+from splat_py.splat import splat
+from splat_py.utils import (
+    inverse_sigmoid,
+    quaternion_to_rotation_torch,
+    transform_points_torch,
+)
 
 
 class GSTrainer:
-    def __init__(self, gaussians):
+    def __init__(self, gaussians, images, cameras):
         self.gaussians = gaussians
+        self.images = images
+        self.cameras = cameras
+
+        self.ssim = StructuralSimilarityIndexMeasure(data_range=1.0).to(
+            self.gaussians.xyz.device
+        )
+
         self.update_optimizer(iter_num=0)
         self.reset_grad_accum()
+        self.setup_test_train_split()
+
+    def setup_test_train_split(self):
+        num_images = len(self.images)
+        all_images = np.arange(num_images)
+        self.test_split = np.arange(0, num_images, TEST_SPLIT_RATIO)
+        self.train_split = np.array(list(set(all_images) - set(self.test_split)))
 
     def update_optimizer(self, iter_num):
         print("Updating optimizer")
@@ -330,3 +365,164 @@ class GSTrainer:
 
         self.reset_grad_accum()
         self.check_nans()
+
+    def compute_test_psnr(self):
+        with torch.no_grad():
+            test_psnrs = []
+            for test_img_idx in self.test_split:
+                test_world_T_image = self.images[test_img_idx].world_T_image
+                test_camera = self.cameras[self.images[test_img_idx].camera_id]
+
+                test_image, _ = splat(self.gaussians, test_world_T_image, test_camera)
+                gt_image = (
+                    self.images[test_img_idx].image.to(torch.float32)
+                    / SATURATED_PIXEL_VALUE
+                )
+                gt_image = gt_image.to(torch.device("cuda"))
+                l2_loss = torch.nn.functional.mse_loss(test_image, gt_image)
+                psnr = -10 * torch.log10(l2_loss).item()
+                test_psnrs.append(psnr)
+        return torch.tensor(test_psnrs)
+
+    def splat_and_compute_loss(self, image_idx, world_T_image, camera):
+        with SimpleTimer("Splat Gaussians"):
+            xyz_camera_frame = transform_points_torch(self.gaussians.xyz, world_T_image)
+            uv = CameraPointProjection.apply(xyz_camera_frame, camera.K)
+            uv.retain_grad()
+
+            # perform frustrum culling
+            culling_mask = torch.zeros(
+                xyz_camera_frame.shape[0],
+                dtype=torch.bool,
+                device=self.gaussians.xyz.device,
+            )
+            culling_mask = culling_mask | (xyz_camera_frame[:, 2] < NEAR_THRESH)
+            culling_mask = (
+                culling_mask
+                | (uv[:, 0] < -1 * CULL_MASK_PADDING)
+                | (uv[:, 0] > camera.width + CULL_MASK_PADDING)
+                | (uv[:, 1] < -1 * CULL_MASK_PADDING)
+                | (uv[:, 1] > camera.height + CULL_MASK_PADDING)
+            )
+
+            # cull gaussians outside of camera frustrum
+            culled_uv = uv[~culling_mask, :]
+            culled_xyz_camera_frame = xyz_camera_frame[~culling_mask, :]
+
+            culled_gaussians = Gaussians(
+                xyz=self.gaussians.xyz[~culling_mask, :],
+                quaternions=self.gaussians.quaternions[~culling_mask, :],
+                scales=self.gaussians.scales[~culling_mask, :],
+                opacities=torch.sigmoid(
+                    self.gaussians.opacities[~culling_mask]
+                ),  # apply sigmoid activation to opacities
+                rgb=torch.sigmoid(
+                    self.gaussians.rgb[~culling_mask, :]
+                ),  # apply sigmoid activation to rgb
+            )
+            sigma_world = ComputeSigmaWorld.apply(
+                culled_gaussians.quaternions, culled_gaussians.scales
+            )
+            J = ComputeProjectionJacobian.apply(culled_xyz_camera_frame, camera.K)
+            sigma_image = ComputeSigmaImage.apply(sigma_world, J, world_T_image)
+
+            # perform tile culling
+            tiles = Tiles(camera.height, camera.width, culled_uv.device)
+            (
+                gaussian_idx_by_splat_idx,
+                splat_start_end_idx_by_tile_idx,
+                tile_idx_by_splat_idx,
+            ) = match_gaussians_to_tiles_gpu(
+                culled_uv, tiles, sigma_image, mh_dist=MH_DIST
+            )
+
+            sorted_gaussian_idx_by_splat_idx = sort_gaussians(
+                culled_xyz_camera_frame,
+                gaussian_idx_by_splat_idx,
+                tile_idx_by_splat_idx,
+            )
+            image = RenderImage.apply(
+                culled_gaussians.rgb,
+                culled_gaussians.opacities,
+                culled_uv,
+                sigma_image,
+                splat_start_end_idx_by_tile_idx,
+                sorted_gaussian_idx_by_splat_idx,
+                torch.tensor([camera.height, camera.width], device=culled_uv.device),
+            )
+        gt_image = (
+            self.images[image_idx].image.to(torch.float32) / SATURATED_PIXEL_VALUE
+        )
+        gt_image = gt_image.to(torch.device("cuda"))
+
+        l1_loss = torch.nn.functional.l1_loss(image, gt_image)
+
+        # print PSNR
+        l2_loss = torch.nn.functional.mse_loss(image, gt_image)
+        psnr = -10 * torch.log10(l2_loss)
+
+        # channel first tensor for SSIM
+        ssim_loss = 1.0 - self.ssim(
+            image.unsqueeze(0).permute(0, 3, 1, 2),
+            gt_image.unsqueeze(0).permute(0, 3, 1, 2),
+        )
+        loss = (1.0 - SSIM_RATIO) * l1_loss + SSIM_RATIO * ssim_loss
+        with SimpleTimer("Backward"):
+            loss.backward()
+        with SimpleTimer("Optimizer Step"):
+            self.optimizer.step()
+
+        self.uv_grad_accum += torch.abs(uv.grad.detach())
+        self.xyz_grad_accum += torch.abs(self.gaussians.xyz.grad.detach())
+        self.grad_accum_count += (~culling_mask).int()
+
+        return image, psnr
+
+    def train(self):
+        for i in range(NUM_ITERS):
+            image_idx = np.random.choice(self.train_split)
+            world_T_image = self.images[image_idx].world_T_image
+            camera = self.cameras[self.images[image_idx].camera_id]
+
+            self.optimizer.zero_grad()
+
+            image, psnr = self.splat_and_compute_loss(image_idx, world_T_image, camera)
+
+            if i % PRINT_INTERVAL == 0:
+                print(
+                    "Iter: {}, PSNR: {}, N: {}".format(
+                        i, psnr.detach().cpu().numpy(), self.gaussians.xyz.shape[0]
+                    )
+                )
+
+            # make sure to perform test split eval before adaptive control
+            # if adaptive control occurs in the same iteration, test psnr will be low
+            if i % TEST_EVAL_INTERVAL == 0:
+                test_psnrs = self.compute_test_psnr()
+                print("\tTEST SPLIT PSNR: ", test_psnrs.mean().item())
+
+            if (
+                i > ADAPTIVE_CONTROL_START
+                and i % ADAPTIVE_CONTROL_INVERVAL == 0
+                and i < ADAPTIVE_CONTROL_END
+            ):
+                self.adaptive_density_control()
+
+            if (
+                i > RESET_OPACTIY_START
+                and i < RESET_OPACITY_END
+                and i % RESET_OPACITY_INTERVAL == 0
+            ):
+                self.reset_opacities(i)
+
+            if i % SAVE_DEBUG_IMAGE_INTERVAL == 0:
+                with SimpleTimer("Save Images"):
+                    debug_image = image.clip(0, 1).detach().cpu().numpy()
+                    cv2.imwrite(
+                        "{}/iter{}_image_{}.png".format(OUTPUT_DIR, i, image_idx),
+                        (debug_image * SATURATED_PIXEL_VALUE).astype(np.uint8)[
+                            ..., ::-1
+                        ],
+                    )
+        final_psnrs = self.compute_test_psnr()
+        print("Final PSNR: ", final_psnrs.mean().item())
