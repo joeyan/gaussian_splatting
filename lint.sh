ab217ed1a11f8b08f382f5a4534ea9f0960b42fd
Joe Yan <yanzhizhong@gmail.com>
2024-03-29 19:21:23 -0400

    minor changes to linter

--- lint.sh
+++ lint.sh
@@ -7,21 +7,21 @@
 set -eo pipefail
 
 # get git root dir
-gitroot=$(git rev-parse --show-toplevel)
+GITROOT=$(git rev-parse --show-toplevel)
 
 # run python formatter in the git root directory
-(cd $gitroot && black .)
+(cd $GITROOT && black .)
 
 # get all c/c++/cuda files in the git src directory
-srcdir = $gitroot/src
-files=$(find $srcdir -type f -name "*.c" -o -name "*.cpp" -o -name "*.cu" -o -name "*.h" -o -name "*.hpp" -o -name "*.cuh")
+SRCDIR=$GITROOT/src
+FILES=$(find $SRCDIR -type f -name "*.c" -o -name "*.cpp" -o -name "*.cu" -o -name "*.h" -o -name "*.hpp" -o -name "*.cuh")
 
-for file in $files; do
-    echo "Linting $file"
+for FILE in $FILES; do
+    echo "Linting $FILE"
     # Replace "#pragma unroll" by "//#pragma unroll"
-    sed -i 's/#pragma unroll/\/\/#pragma unroll/g' $file
+    sed -i 's/#pragma unroll/\/\/#pragma unroll/g' $FILE
     # Do format
-    clang-format -i $file
+    clang-format -i $FILE
     # Replace "// *#pragma unroll" by "#pragma unroll"
-    sed -i 's/\/\/ *#pragma unroll/#pragma unroll/g' $file
+    sed -i 's/\/\/ *#pragma unroll/#pragma unroll/g' $FILE
 done
